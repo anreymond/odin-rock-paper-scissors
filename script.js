04875c22b56fcf900ce250d247437e206c9a6f97
anreymon <anreymon@gmail.com>
2025-07-28 00:10:46 +0200

    Add the function playRound() to simulate a round.
    The function takes two parameters: the player's move and the computer's move. It logs the result of the round and increments the scores accordingly.

--- script.js
+++ script.js
@@ -17,8 +17,76 @@ function getHumanChoice() {
     return prompt("Make your next move!", "rock, paper or scissors");
 }
 
-console.log("Player: " + getHumanChoice());
-console.log("Computer: " + getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("It's a tie!");
+                    break;
+                case "paper":
+                    computerScore++;
+                    console.log("You lose! Paper beats Rock");
+                    break;
+                case "scissors":
+                    humanScore++;
+                    console.log("You win! Rock beats Scissors");
+                    break;
+                default:
+                    console.log("unexpected");
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    humanScore++;
+                    console.log("You win! Paper beats Rock");
+                    break;
+                case "paper":
+                    console.log("It's a tie!");
+                    break;
+                case "scissors":
+                    computerScore++;
+                    console.log("You lose! Scissors beat Paper");
+                    break;
+                default:
+                    console.log("unexpected");
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    computerScore++;
+                    console.log("You lose! Rock beats Scissors");
+                    break;
+                case "paper":
+                    humanScore++
+                    console.log("You win! Scissors beat Paper");
+                    break;
+                case "scissors":
+                    console.log("It's a tie!");
+                    break;
+                default:
+                    console.log("unexpected");
+                    break;
+            }
+            break;
+        default:
+            console.log("Please make a valid move.");
+            break;
+    }
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 
 
