7e90c9a0269f58dcbbe939160a69a695a60defb4
anreymon <anreymon@gmail.com>
2025-07-31 11:33:10 +0200

    Add a UI to the game
    Add buttons to let the player choose his next move. The result of the round appears on the page and the current score is updated.
    The game stops when a someone wins 5 times.

--- script.js
+++ script.js
@@ -1,99 +1,88 @@
 function getComputerChoice() {
     let rand = Math.floor(3 * Math.random());
+    let moves = ["rock", "paper", "scissors"];
+    return moves[rand];
+}
 
-    switch (rand) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
+let scores = [0, 0];
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => button.addEventListener("click", playRound));
+
+function playRound(e) {
+    let humanChoice = e.target.id;
+    let computerChoice = getComputerChoice();
+    let resDiv = document.querySelector("#results");
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    resDiv.textContent = "It's a tie!";
+                    break;
+                case "paper":
+                    scores[1]++;
+                    resDiv.textContent = "You lose! Paper beats Rock";
+                    break;
+                case "scissors":
+                    scores[0]++;
+                    resDiv.textContent = "You win! Rock beats Scissors";
+                    break;
+                default:
+                    console.log("unexpected");
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    scores[0]++;
+                    resDiv.textContent = "You win! Paper beats Rock";
+                    break;
+                case "paper":
+                    resDiv.textContent = "It's a tie!";
+                    break;
+                case "scissors":
+                    scores[1]++;
+                    resDiv.textContent = "You lose! Scissors beat Paper";
+                    break;
+                default:
+                    console.log("unexpected");
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    scores[1]++;
+                    resDiv.textContent = "You lose! Rock beats Scissors";
+                    break;
+                case "paper":
+                    scores[0]++;
+                    resDiv.textContent = "You win! Scissors beat Paper";
+                    break;
+                case "scissors":
+                    resDiv.textContent = "It's a tie!";
+                    break;
+                default:
+                    console.log("unexpected");
+                    break;
+            }
+            break;
         default:
-            return "unexpected";
+            console.log("Please make a valid move.");
+            break;
+    }
+    let playerScore = document.querySelector("#player-score");
+    let computerScore = document.querySelector("#computer-score");
+    playerScore.textContent = scores[0];
+    computerScore.textContent = scores[1];
+    if (scores[0] >= 5 || scores[1] >= 5) {
+        buttons.forEach(button => button.remove());
+        let body = document.querySelector("body");
+        let finalRes = document.createElement("div");
+        finalRes.textContent = scores[0] >= 5 ? "You won the game!" : "You lost the game...";
+        body.appendChild(finalRes);
     }
 }
 
-function getHumanChoice() {
-    return prompt("Make your next move!", "").toLowerCase();
-}
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore < computerScore) {
-        console.log("You lost the game... Final score: " + humanScore + "-" + computerScore);
-    } else if (humanScore > computerScore) {
-        console.log("You won the game! Final score: " + humanScore + "-" + computerScore);
-    } else {
-        console.log("It's a tie! Final score: " + humanScore + "-" + computerScore);
-    }
-    
-    function playRound(humanChoice, computerChoice) {
-        switch (humanChoice) {
-            case "rock":
-                switch (computerChoice) {
-                    case "rock":
-                        console.log("It's a tie!");
-                        break;
-                    case "paper":
-                        computerScore++;
-                        console.log("You lose! Paper beats Rock");
-                        break;
-                    case "scissors":
-                        humanScore++;
-                        console.log("You win! Rock beats Scissors");
-                        break;
-                    default:
-                        console.log("unexpected");
-                        break;
-                }
-                break;
-            case "paper":
-                switch (computerChoice) {
-                    case "rock":
-                        humanScore++;
-                        console.log("You win! Paper beats Rock");
-                        break;
-                    case "paper":
-                        console.log("It's a tie!");
-                        break;
-                    case "scissors":
-                        computerScore++;
-                        console.log("You lose! Scissors beat Paper");
-                        break;
-                    default:
-                        console.log("unexpected");
-                        break;
-                }
-                break;
-            case "scissors":
-                switch (computerChoice) {
-                    case "rock":
-                        computerScore++;
-                        console.log("You lose! Rock beats Scissors");
-                        break;
-                    case "paper":
-                        humanScore++
-                        console.log("You win! Scissors beat Paper");
-                        break;
-                    case "scissors":
-                        console.log("It's a tie!");
-                        break;
-                    default:
-                        console.log("unexpected");
-                        break;
-                }
-                break;
-            default:
-                console.log("Please make a valid move.");
-                break;
-        }
-    }
-}
-
-playGame();
